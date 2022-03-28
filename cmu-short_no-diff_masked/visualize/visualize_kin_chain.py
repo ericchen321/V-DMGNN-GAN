c33f33c7c5c305aea17806d985d8721e62d09387
eric <cgx1997@outlook.com>
2022-03-27 22:52:47 -0700

    fixed kin tree; moved bone definition around

--- cmu-short_no-diff_masked/visualize/visualize_kin_chain.py
+++ cmu-short_no-diff_masked/visualize/visualize_kin_chain.py
@@ -17,6 +17,15 @@ def compute_affine_xform_mat(sample_index, time_index, joint_index, joint_rotmat
         sample_index, time_index, joint_index, :, :
     ]
     xform_mat_4d[3, 3] = 1.0
+    #trans_4d = np.eye(4)
+    #trans_4d[0:3, 3] = np.array([0.0, 0.0, bone_length])
+    #rot_4d = np.eye(4)
+    #rot_4d[0:3, 0:3] = joint_rotmats[
+    #    sample_index, time_index, joint_index, :, :
+    #]
+    #xform_mat_4d = np.matmul(
+    #    rot_4d, trans_4d
+    #)
 
     # compute transform matrix from parent to world
     if parents[joint_index] == -1:
@@ -99,10 +108,33 @@ def compute_joint_pos(joint_exps, parents, bone_length):
 
 if __name__ == '__main__':
     # define the kinematic tree on the CMU dataset
-    # (26 joints, not 38 joints)
     cmu_parents = np.array([
-        8, 0, 1, 2, 8, 4, 5, 6, 9, 10, 11, -1, 11, 12, 10, 14, 15, 16, 17, 16, 11, 20, 21, 22, 23, 22
+    #    8, 0, 1, 2, 8, 4, 5, 6, 9, 10, 11, -1, 11, 12, 10, 14, 15, 16, 17, 16, 11, 20, 21, 22, 23, 22
+    #    8, 0, 1, 2, 8, 4, 5, 6, -1, 8, 9, 10, 11, 12, 10, 14, 15, 16, 17, 16, 11, 20, 21, 22, 23, 22
+        -1, 0, 1, 2, 8, 4, 5, 6, 0, 8, 9, 10, 11, 12, 10, 14, 15, 16, 17, 16, 11, 20, 21, 22, 23, 22
     ])
+
+    # define bones
+    neighbor_link_partition = {
+        "left_arm": [
+            (10, 14), (14, 15), (15, 16), (16, 19), (16, 17), (17, 18)
+        ],
+        "right_arm": [
+            (11, 20), (20, 21), (21, 22), (22, 23), (23, 24), (23, 25)
+        ],
+        "torso": [
+            (8, 9), (9, 10), (10, 11)
+        ],
+        "head": [
+            (11, 12), (12, 13)
+        ],
+        "left_leg": [
+            (0, 8), (0, 1), (1, 2), (2, 3)
+        ],
+        "right_leg": [
+            (4, 8), (4, 5), (5, 6), (6, 7)
+        ]
+    }
 
     # define bone length for visualization
     bone_length = 2.0
@@ -110,7 +142,7 @@ if __name__ == '__main__':
     # convert joint angles to joint positions in
     # the world frame
     encoder_inputs_4d = np.load(
-        "/home/eric/eece571f/DMGNN/cmu-short_no-diff_masked/visualize/encoder_inputs.npy",
+        "/home/eric/eece571f/DMGNN/cmu-short_no-diff_masked/visualize/encoder_inputs_walking.npy",
     )
     encoder_inputs_cartesian = compute_joint_pos(
         encoder_inputs_4d,
@@ -123,27 +155,6 @@ if __name__ == '__main__':
     for time_id in range(encoder_inputs_cartesian.shape[1]):
         fig = plt.figure(figsize=(12, 12))
         ax = fig.add_subplot(projection='3d')
-        # build bones
-        neighbor_link_partition = {
-            "left_arm": [
-                (10, 14), (14, 15), (15, 16), (16, 19), (16, 17), (17, 18)
-            ],
-            "right_arm": [
-                (11, 20), (20, 21), (21, 22), (22, 23), (23, 24), (23, 25)
-            ],
-            "torso": [
-                (8, 9), (9, 10), (10, 11)
-            ],
-            "head": [
-                (11, 12), (12, 13)
-            ],
-            "left_leg": [
-                (0, 8), (0, 1), (1, 2), (2, 3)
-            ],
-            "right_leg": [
-                (4, 8), (4, 5), (5, 6), (6, 7)
-            ]
-        }
         encoder_inputs_sample_x_time_t = encoder_inputs_cartesian[4, time_id, :].reshape(-1, 3) # J, 3
         #print(encoder_inputs_sample_x_time_t.shape)
         #plot joints
@@ -179,6 +190,6 @@ if __name__ == '__main__':
                 f"{joint_index}",
                 fontsize = 12,
                 color="blue")
-        plt.savefig(f"skeletons/skeleton_<{time_id}>.png")
+        plt.savefig(f"skeletons_walking/skeleton_<{time_id}>.png")
         plt.close(fig)
     print(f"plotted {encoder_inputs_cartesian.shape[1]} skeletons")
